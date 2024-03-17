ad5c83f719696cda6ec9296527c7e980b84b83a9
Egor Kostiuk <127864304+EgorKostiuk@users.noreply.github.com>
2024-03-17 19:35:17 +0200

    + add dark theme css

--- js/dark-theme.js
+++ js/dark-theme.js
@@ -2,4 +2,5 @@ const btnDarkMode = document.querySelector('.dark-mode-btn');
 
 btnDarkMode.onclick = function() {
   btnDarkMode.classList.toggle('dark-mode-btn--active');
+  document.body.classList.toggle('dark')
 }
