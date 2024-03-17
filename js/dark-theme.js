af8de93a4f603985f2ca3ffedd110c9f21b056f5
Egor Kostiuk <127864304+EgorKostiuk@users.noreply.github.com>
2024-03-17 19:47:42 +0200

    + implement dark theme JS script

--- js/dark-theme.js
+++ js/dark-theme.js
@@ -1,6 +1,16 @@
 const btnDarkMode = document.querySelector('.dark-mode-btn');
 
+// checking theme in localStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+  btnDarkMode.classList.add('dark-mode-btn--active');
+  document.body.classList.add('dark');
+}
+
+// turning on dark mode
 btnDarkMode.onclick = function() {
   btnDarkMode.classList.toggle('dark-mode-btn--active');
-  document.body.classList.toggle('dark')
+  const isDark = document.body.classList.toggle('dark');
+
+  // add theme to localStorage
+  isDark ? localStorage.setItem('darkMode', 'dark') : localStorage.setItem('darkMode', 'light')
 }
