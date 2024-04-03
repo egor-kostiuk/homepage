5b65a2d71c9979b3fc78186ac9ab639ca76bfb68
Egor Kostiuk <kostukegor729@gmail.com>
2024-04-03 19:55:54 +0300

    * fix footer icons for projects

--- js/footer/footer-social.js
+++ js/footer/footer-social.js
@@ -2,6 +2,8 @@ export function createSocialItem(data) {
     const socialList = document.createElement('ul');
     socialList.classList.add('footer__social');
 
+    const check = document.querySelector('.check'); // ! optimize (as soon as possible)
+
     for (let key in data) {
         const socialItem = document.createElement('li');
         const socialLink = document.createElement('a');
@@ -12,19 +14,39 @@ export function createSocialItem(data) {
 
         switch (key) {
             case 'telegram':
-                socialIcon.src = 'img/icons/telegram.svg';
+                if (check !== null) { // optimize
+                    socialIcon.src = '../img/icons/telegram.svg';
+                } else {
+                    socialIcon.src = 'img/icons/telegram.svg';
+                }
                 break;
             case 'instagram':
-                socialIcon.src = 'img/icons/instagram.svg';
+                if (check !== null) { // optimize
+                    socialIcon.src = '../img/icons/instagram.svg';
+                } else {
+                    socialIcon.src = 'img/icons/instagram.svg';
+                }
                 break;
             case 'twitter':
-                socialIcon.src = 'img/icons/twitter.svg';
+                if (check !== null) { // optimize
+                    socialIcon.src = '../img/icons/twitter.svg';
+                } else {
+                    socialIcon.src = 'img/icons/twitter.svg';
+                }
                 break;
             case 'github':
-                socialIcon.src = 'img/icons/github.svg';
+                if (check !== null) { // optimize
+                    socialIcon.src = '../img/icons/github.svg';
+                } else {
+                    socialIcon.src = 'img/icons/github.svg';
+                }
                 break;
             case 'linkedin':
-                socialIcon.src = 'img/icons/linkedin.svg';
+                if (check !== null) { // optimize
+                    socialIcon.src = '../img/icons/linkedin.svg';
+                } else {
+                    socialIcon.src = 'img/icons/linkedin.svg';
+                }
                 break;
             default:
                 socialIcon.src = 'img/icons';
