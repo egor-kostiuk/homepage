f72480f4e7ad6f1d28a280b10f9b8368ebf5f195
Egor Kostiuk <kostukegor729@gmail.com>
2024-04-03 14:26:55 +0300

    * fix social list position in footer__wrapper

--- js/footer/footer.js
+++ js/footer/footer.js
@@ -4,6 +4,8 @@ import { container } from './footer-container.js';
 
 const footer = document.querySelector('.footer');
 
+// adds the main container to the footer
 footer.appendChild(container);
 
-container.children[0].appendChild(createSocialItem(social));
+// add a social list to the first child (footer__wrapper) of the container
+container.children[0].insertBefore(createSocialItem(social), container.children[0].firstChild);
