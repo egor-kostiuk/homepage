de4fa7c83fb23c86f8f2ea7aabca208b0d297dac
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-06 12:04:00 +0300

    feat: add skills & biography pages

---
+++ src/pages/SkillsPage.jsx
@@ -0,0 +1,19 @@
+export const SkillsPage = () => {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Skills</h1>
+        <ul className="content__list">
+          <li className="content__list-item">
+            <h2 className="title-2">Tech Stack</h2>
+            <p>JavaScript, HTML5, CSS3, ReactJS, Webpack, Vite, Git (GitHub), MySQL</p>
+          </li>
+          <li className="content__list-item">
+            <h2 className="title-2">Soft Skills</h2>
+            <p>Communication, Assertiveness, Confidence, Fast learner</p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  )
+}
