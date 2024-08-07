7bd9157e3c84fc888fd59deddda86bc734a9c14a
Egor Kostiuk <kostukegor729@gmail.com>
2024-08-07 08:55:58 +0300

    update: utils js to ts

--- src/utils/scrollToTop.ts
+++ src/utils/scrollToTop.ts
@@ -1,7 +1,8 @@
+import React from "react";
 import {useEffect} from "react";
 import {useLocation} from "react-router-dom";
 
-export const ScrollToTop = () => {
+export const ScrollToTop: React.FC = () => {
   const {pathname} = useLocation();
 
   useEffect(() => {
