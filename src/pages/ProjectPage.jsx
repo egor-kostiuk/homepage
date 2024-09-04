c950609ed63a00e1114d641826d3ee0301635b47
Egor Kostiuk <kostukegor729@gmail.com>
2024-09-04 12:30:50 +0300

    fix: project page

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -11,28 +11,22 @@ export const ProjectPage = () => {
 
   useFavicon(project.icon);
 
-  console.log(project);
-
   return (
     <main className="section">
       <div className="container">
         <div className="project__wrapper">
           <h1 className="title-1">{project.title}</h1>
-
           <div className={project.className}>
             <img src={project.img} alt="project photo" className="project__img"/>
           </div>
-
-          <div className="project__descr">
+          <div className="project__description">
             <p>Skills: HTML, CSS, JavaScript</p>
           </div>
-
           <div className="buttons">
             <a href={project.gitHubLink} className="btn-outline" target={"_blank"}>
               <img src={gitHubIcon} alt="code"/>
                 GitHub repo
             </a>
-
             {project.demoLink && (
               <a href={project.demoLink} className="btn-outline" target={"_blank"}>
                 <img src={netlifyIcon} alt="demo"/>
@@ -40,7 +34,6 @@ export const ProjectPage = () => {
               </a>
             )}
           </div>
-
         </div>
       </div>
     </main>
