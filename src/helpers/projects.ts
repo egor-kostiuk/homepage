5defbaab7b99a2002be1e4f32ba6c01820cd8443
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-28 12:34:16 +0200

    feaat: add projects assets

---
+++ src/helpers/projects.ts
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    title: 'Travel Service',
+    skills: 'HTML, CSS, JavaScript',
+    img: '/src/assets/img/projects/project_1.png',
+    gitHubLink: 'https://github.com/EgorKostiuk/travel-project',
+    demoLink: 'https://travel-project-japan.netlify.app',
+  },
+  {
+    title: 'Registration',
+    skills: 'HTML, CSS, JavaScript',
+    img: '/src/assets/img/projects/project_2.png',
+    gitHubLink: 'https://github.com/EgorKostiuk/loginform',
+    demoLink: 'https://egorkostiuk.github.io/loginform/',
+  },
+  {
+    title: 'YourMeal',
+    skills: 'HTML, CSS, JavaScript',
+    img: '/src/assets/img/projects/project_3.png',
+    gitHubLink: 'https://github.com/EgorKostiuk/YourMeal',
+  }
+]
