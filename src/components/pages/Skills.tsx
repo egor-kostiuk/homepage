a2d6c955d05cd51a5b6a072d4e42a6fc840e385e
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-28 18:39:30 +0200

    update: Skills page

--- src/components/pages/Skills.tsx
+++ src/components/pages/Skills.tsx
@@ -1,31 +1,30 @@
 import { FC } from 'react'
 
-import { Nav } from '../ui/nav/Nav'
-import { Footer } from '../ui/footer/Footer'
+import { Layout } from '../ui/layout/Layout'
 
 export const Skills: FC = () => {
 	return (
-    <>
-    <Nav/>
-		<main className="section">
-			<div className="container">
-				<h1 className="title-1">Skills</h1>
-				<ul className="content__list">
-					<li className="content__list-item">
-						<h2 className="title-2">Tech Stack</h2>
-						<p>
-							JavaScript, TypeScript, HTML5, CSS3, SCSS, Tailwind, React Query,
-							ReactJS , Git (GitHub), PostgreSQL
-						</p>
-					</li>
-					<li className="content__list-item">
-						<h2 className="title-2">Soft Skills</h2>
-						<p>Communication, Assertiveness, Confidence, Fast learner</p>
-					</li>
-				</ul>
-			</div>
-		</main>
-    <Footer/>
-    </>
+		<>
+			<Layout>
+				<main className="section">
+					<div className="container">
+						<h1 className="title-1">Skills</h1>
+						<ul className="content__list">
+							<li className="content__list-item">
+								<h2 className="title-2">Tech Stack</h2>
+								<p>
+									JavaScript, TypeScript, HTML5, CSS3, SCSS, Tailwind, React
+									Query, ReactJS , Git (GitHub), PostgreSQL
+								</p>
+							</li>
+							<li className="content__list-item">
+								<h2 className="title-2">Soft Skills</h2>
+								<p>Communication, Assertiveness, Confidence, Fast learner</p>
+							</li>
+						</ul>
+					</div>
+				</main>
+			</Layout>
+		</>
 	)
 }
