1698b1093cb6d746609ee858dda5b4277cd59655
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-28 18:42:51 +0200

    feat: add main content styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
 
 import './App.css'
+import './project-details.css' // TODO: optimize
 
 export const App = () => {
 	return (
