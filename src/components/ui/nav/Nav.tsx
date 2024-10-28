e6bcc1fb400e19026b997c6e53d418e291ce1d6f
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-28 11:54:36 +0200

    feat: add Nav

---
+++ src/components/ui/nav/Nav.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+// import { DarkModeButton } from '../Buttons/DarkModeButton';
+
+import './Nav.css';
+
+
+export const Nav: React.FC = () => {
+  const activeLink = 'nav__list-link nav__list-link--active'
+  const normalLink = 'nav__list-link'
+
+  return (
+    <nav className="nav">
+      <div className="container">
+        <div className="nav__row">
+          <NavLink to={'/'} className={'nav__title'}>
+            <strong>Developer </strong>
+            <span>portfolio</span>
+          </NavLink>
+          {/* <DarkModeButton/> */}
+          <ul className="nav__list">
+            <li className="nav___list-item">
+              <NavLink to={'/'} className={({isActive}) => isActive ? activeLink : normalLink}>
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav___list-item">
+              <NavLink to={'/skills'} className={({isActive}) => isActive ? activeLink : normalLink}>
+                Skills
+              </NavLink>
+            </li>
+            <li className="nav___list-item">
+              <NavLink to={'/contacts'} className={({isActive}) => isActive ? activeLink : normalLink}>
+                Contacts
+              </NavLink>
+            </li>
+            <li className="nav___list-item">
+              <NavLink to={'/biography'} className={({isActive}) => isActive ? activeLink : normalLink}>
+                Biography
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
