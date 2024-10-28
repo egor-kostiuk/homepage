907376b0cfa0752874e7b1fc267db6608032cee7
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-28 11:56:12 +0200

    feat: add Header

---
+++ src/components/ui/header/Header.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+
+import './Header.css'
+
+export const Header: FC = () => {
+	return (
+		<header className="header">
+			<div className="header__wrapper">
+				<h1 className="header__title">
+					<strong>
+						Hi, my name is <em>Egor</em>
+					</strong>
+					<br />
+					<span>a full stack developer</span>
+				</h1>
+				<div className="header__text">
+					<p>with passion for learning and creating.</p>
+				</div>
+				<a
+					href="https://github.com/egor-kostiuk"
+					className="btn"
+					target={'_blank'}
+				>
+					View GitHub
+				</a>
+			</div>
+		</header>
+	)
+}
