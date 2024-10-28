d23852223b93cb1e5d02de6f59a5a768c1b03282
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-28 11:54:02 +0200

    feat: add social assets

---
+++ src/helpers/social.ts
@@ -0,0 +1,27 @@
+export const social = [
+  {
+    name: 'telegram',
+    link: 'https://t.me/egor_kostiuk',
+    img: '/src/assets/icons/social/telegram.svg'
+  },
+  {
+    name: 'instagram',
+    link: 'https://www.instagram.com/_ve1ry/',
+    img: '/src/assets/icons/social/instagram.svg'
+  },
+  {
+    name: 'twitter',
+    link: 'https://x.com/ve1ry_',
+    img: '/src/assets/icons/social/twitter.svg'
+  },
+  {
+    name: 'gitHub',
+    link: 'https://github.com/egor-kostiuk',
+    img: '/src/assets/icons/social/github.svg'
+  },
+  {
+    name: 'linkedIn',
+    link: 'https://www.linkedin.com/in/egor-kostiuk-02912127a/',
+    img: '/src/assets/icons/social/linkedin.svg'
+  }
+]
