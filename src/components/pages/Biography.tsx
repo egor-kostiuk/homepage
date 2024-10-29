5621c1eb0cd1b091a1db7b918e35bf11ddea8725
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-29 10:55:43 +0200

    feat: add Biography page

---
+++ src/components/pages/Biography.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import { Layout } from '../ui/layout/Layout'
+
+export const Biography: FC = () => {
+	return (
+		<Layout>
+			<main className="section">
+				<div className="container">
+					<h1 className="title-1">Biography</h1>
+				</div>
+			</main>
+		</Layout>
+	)
+}
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
+
 import { Home } from '../components/pages/Home'
 import { Skills } from '../components/pages/Skills'
 import { Contacts } from '../components/pages/Contacts'
+import { Biography } from '../components/pages/Biography'
 
 export const router = createBrowserRouter([
 	{
@@ -18,6 +20,6 @@ export const router = createBrowserRouter([
 	},
 	{
 		path: '/biography',
-		// element: <Biography/>
+		element: <Biography/>
 	}
 ])
