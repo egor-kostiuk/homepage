cca645824db8a303302d94164e51906c0a26bbbc
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-30 23:21:26 +0200

    update: styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
 
-import './styles/App.css'
-import './styles/project-details.css' // TODO: optimize
-import './styles/colors.css'
-
 export const App = () => {
 	return (
 		<div className="App">
